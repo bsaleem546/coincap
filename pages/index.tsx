c38746853826d09963a1b29bd110c76230efb49b
bsaleem546 <m.bilal2145@gmail.com>
2022-06-08 13:10:03 +0500

    completed swap not working

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import type { NextPage } from 'next'
 import Header from '../components/Header'
 import Trending from '../components/Trending'
 import CMCTable from '../components/cmc-table/cmcTable'
+import SwapCryptoModal from '../components/swapCryptoModal'
 
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen'>
 
         <Header />
+
+        <SwapCryptoModal />
 
         <div className='mt-10' />
 
